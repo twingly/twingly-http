93d93936e058219c99dd3d4aac6164246de77ad4
Pontus <pontus.svensson@twingly.com>
2020-11-20 10:53:32 +0100

    Make logger optional in client
    ...and add corresponding spec
    
    Close #9

--- spec/lib/twingly/http_spec.rb
+++ spec/lib/twingly/http_spec.rb
@@ -3,13 +3,12 @@
 class CustomError < StandardError; end
 
 RSpec.describe Twingly::HTTP::Client do
-  let(:logger)            { NullLogger.new }
   let(:url)               { "http://example.org/" }
   let(:base_user_agent)   { "Twingly::HTTP/1.0" }
 
   let(:client) do
     client =
-      described_class.new(logger: logger, base_user_agent: base_user_agent)
+      described_class.new(base_user_agent: base_user_agent)
 
     client.request_id = request_id if defined?(request_id)
 
@@ -309,6 +308,15 @@ RSpec.describe Twingly::HTTP::Client do
       let(:commit)     { "b5c40853a18a29982d65034be2ed53698112f60f" }
       let(:release)    { "v112" }
 
+      let(:client) do
+        client =
+          described_class.new(base_user_agent: base_user_agent, logger: logger)
+
+        client.request_id = request_id if defined?(request_id)
+
+        client
+      end
+
       describe "request" do
         let(:expected_log_row) do
           "at=info " \
@@ -464,6 +472,21 @@ RSpec.describe Twingly::HTTP::Client do
               .to raise_error(Twingly::HTTP::ConnectionError)
           end
         end
+      end
+    end
+  end
+
+  describe "#initialize" do
+    let(:client) { described_class.new(base_user_agent: base_user_agent) }
+
+    context "when no logger is given" do
+      it "defaults to a Logger" do
+        expect(client.logger).to be_a(Logger)
+      end
+
+      it "defaults to 'dev/null'" do
+        expect { client.logger.info("foo") }
+          .to_not output(/foo/).to_stdout
       end
     end
   end
